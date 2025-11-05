1234318426456950387252cbaa023ac9f090692b
Paulo Felippe <paulo.felippe222@gmail.com>
2025-11-05 16:15:32 -0300

    Registra a troca de estado num arquivo de texto para consulta posterior

---
+++ keepalived_notify.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+touch /var/run/keepalived_status
+chmod 0644 /var/run/keepalived_status
+echo "$1 $2 has transitioned to the $3 state with a priority of $4" > /var/run/keepalived_status_$2
